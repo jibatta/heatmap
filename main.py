769a9a6a19a18a63a7ad7ebd422e9de6b25fb78c
Juan Ignacio Battaglino <juanibattaglino@gmail.com>
2019-10-14 20:10:42 -0300

    Add keyboard and pdf functions.

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 from kivy.app import App
+from kivy.core.window import Window
+from kivy.graphics import Ellipse, Line, Color
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.graphics import Ellipse, Line, Color
-from utils import log
+from kivy.uix.behaviors import DragBehavior
+from kivy.uix.vkeyboard import VKeyboard
+from utils import log, pdf_generator
 
 
 class myApplication(Widget):
     def __init__(self, **kwargs):
         super(myApplication, self).__init__(**kwargs)
+        self.my_vkeyboard = VKeyboard()
         self.point_list_figure = []
         self.mode = 'first_step'
         log('heatmap','INFO',"First step: draw area.")
 
     def on_touch_down(self, touch):
-        if self.mode is 'first_step'  and touch.x < self.width / 1.25:
-            self.line_start_point_x, self.line_start_point_y = touch.pos
-        # elif self.mode is 'second_step':
+        if self.mode is 'second_step' and touch.x < self.width / 1.25 and touch.y > self.height / 4:
+            print(self.point_list_figure) 
             # I should select one line from the draw and get its initial and final point to set scale
-            # if inputlabel opened:
-            # I should open virtual keyboard here.
         elif self.mode == 'third_step' and touch.is_double_tap:
             with self.canvas:
                 Color(1, 0, 1) 
@@ -35,7 +36,7 @@ class myApplication(Widget):
         if self.mode is 'first_step' and touch.x < self.width / 1.25:
             with self.canvas:
                 Color(0,0,0)
-                point_list = [self.line_start_point_x, self.line_start_point_y, touch.x, touch.y]
+                point_list = [touch.x, touch.y]
                 self.point_list_figure.append(point_list)
                 Line(points=self.point_list_figure, width = 5)
         else:
@@ -45,18 +46,22 @@ class myApplication(Widget):
         if self.mode is 'first_step':
             self.mode = 'second_step'
             log('heatmap','INFO',"Second step: Select scale.")
+            self.add_widget(self.my_vkeyboard)
         elif self.mode is 'second_step':
             self.mode = 'third_step'
-            self.scale = self.ids.input_scale.text
             log('heatmap','INFO',"The scale is: {}.".format(self.scale))
             log('heatmap','INFO',"Third step: measure wifi signal.")
+            self.remove_widget(self.my_vkeyboard)
         elif self.mode is 'third_step':
+            pdf_generator()
             self.mode = 'fourth_step'
-            # Get measures from database here and load heatmap results.
-            log('heatmap','INFO',"Fourth step: printing results.")
+            log('heatmap','INFO',"Fourth step: generating PDF report.")
         elif self.mode is 'fourth_step':
             log('heatmap','INFO',"Closing application - Bye!")
             App.get_running_app().stop()
+
+    def search_location(self):
+        self.scale = self.ids.input_scale.text
 
 class heatmap(App):
     def build(self):
--- utils.py
+++ utils.py
@@ -1,2 +1,48 @@
+from reportlab.platypus import SimpleDocTemplate, Paragraph, PageBreak
+from reportlab.lib.units import mm, inch
+from reportlab.lib.styles import getSampleStyleSheet
+from io import BytesIO
+
 def log(application, log_level, msg):
     print("[{}] - [{}] - {}".format(application, log_level, msg))
+
+def pdf_generator():
+    # This should be the results of the measures - Hardcoded temporarily.
+    ssid_list = ['UTN BA']
+
+    pdf_buffer = []
+    my_pdf = SimpleDocTemplate('heatmap_report.pdf')
+
+    pdf_style_sheet = getSampleStyleSheet()
+
+    paragraph_1 = Paragraph('Reporte de medición WiFi<br /><br /><br />', pdf_style_sheet['Heading1'])
+    paragraph_2 = Paragraph('Proyecto de Base de Datos 2019 - Heatmap<br /><br />', pdf_style_sheet['Heading3'])
+    paragraph_3 = Paragraph('Alumno: Juan Ignacio Battaglino - juanibattaglino@gmail.com<br /><br />', pdf_style_sheet['Heading3'])
+    paragraph_4 = Paragraph('Profesores: Sergio Kaszczyszyn - Roberto Gomez<br /><br />', pdf_style_sheet['Heading3'])
+    
+    pdf_buffer.append(paragraph_1)
+    pdf_buffer.append(paragraph_2)
+    pdf_buffer.append(paragraph_3)
+    pdf_buffer.append(paragraph_4)
+    pdf_buffer.append(PageBreak())
+
+    # This would be inside a for loop.
+    for ssid in ssid_list:
+        paragraph_measure = Paragraph('SSID: {}'.format(ssid), pdf_style_sheet['Heading2'])
+        pdf_buffer.append(paragraph_measure)
+        # I would insert here the image.
+        # pdf_buffer.append(ssid + '.png')
+        pdf_buffer.append(PageBreak())
+    # Loop would end here.
+
+    my_pdf.build(pdf_buffer, onFirstPage=add_page_number, onLaterPages=add_page_number)
+
+def add_page_number(canvas, doc):
+     canvas.saveState()
+     canvas.setFont('Times-Roman', 10)
+     page_number_text = "%d" % (doc.page)
+     canvas.drawCentredString(0.75 * inch, 0.75 * inch, page_number_text)
+     canvas.restoreState()
+
+if __name__ == "__main__":
+    pdf_generator()
